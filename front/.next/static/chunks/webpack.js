13cf606f51c8f7c6b0105ac1acdb7f47d733eac1
yuntaek ha <yuntaek.ha@gmail.com>
2021-03-01 18:32:57 +0900

    add middleware & redux-devtools-extention

--- front/.next/static/chunks/webpack.js
+++ front/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6118e7e1951041daa7f6";
+/******/ 	var hotCurrentHash = "e7cc7f3b73886ec2fff9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
